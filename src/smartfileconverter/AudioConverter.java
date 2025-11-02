c7578d22164e15b2615abce6e5eb090975f1b5c5
MODALAVALASA RAVI KIRAN <ravikiranmodalavalasa@gmail.com>
2025-11-02 19:55:44 +0530

    Update AudioConverter.java
    added try with resouces block  and aslo used transerTo() method

--- src/smartfileconverter/AudioConverter.java
+++ src/smartfileconverter/AudioConverter.java
@@ -7,3 +7,7 @@ Public class AudioConverter
     public File mockConvert(File input, String targetExt) throws IOException
     {
         File out = new File(input.getParent(),input.getName().replace(".", "_converted."));
+        try (FileInputStream fis = new FileInputStream(input);FileOutputStream fos = new FileOutputStream(out))
+        {
+            fis.transferTo(fos);
+        }
