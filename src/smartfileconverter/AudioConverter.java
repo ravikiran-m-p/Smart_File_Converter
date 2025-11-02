76610a377d3cb369f93c2d66495b709e541afb41
MODALAVALASA RAVI KIRAN <ravikiranmodalavalasa@gmail.com>
2025-11-02 19:53:43 +0530

    spell mistakes cleared and maintianed a ruling convection case

--- src/smartfileconverter/AudioConverter.java
+++ src/smartfileconverter/AudioConverter.java
@@ -2,6 +2,6 @@ package SmartFileConverter.src.smartfileconverter;
 
 import java.io.*;
 
-ublic class AudioConverter {
-
+Public class AudioConverter
+{
     public File mockConvert(File input, String targetExt) throws IOException 
