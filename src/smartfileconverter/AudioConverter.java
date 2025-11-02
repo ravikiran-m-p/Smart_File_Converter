daf5314777513c772ca92be9d728b799a2350424
MODALAVALASA RAVI KIRAN <ravikiranmodalavalasa@gmail.com>
2025-11-02 19:52:26 +0530

    Update AudioConverter.java

--- src/smartfileconverter/AudioConverter.java
+++ src/smartfileconverter/AudioConverter.java
@@ -1,3 +1,7 @@
 package SmartFileConverter.src.smartfileconverter;
 
 import java.io.*;
+
+ublic class AudioConverter {
+
+    public File mockConvert(File input, String targetExt) throws IOException 
