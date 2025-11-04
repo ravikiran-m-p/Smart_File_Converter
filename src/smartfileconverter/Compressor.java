698695d0913821e68432eb7f4ee428e5f88ec4d4
MODALAVALASA RAVI KIRAN <ravikiranmodalavalasa@gmail.com>
2025-11-04 18:45:09 +0530

    Update Compressor.java

--- src/smartfileconverter/Compressor.java
+++ src/smartfileconverter/Compressor.java
@@ -8,4 +8,8 @@ public class Compressor
    public File compressText(File input) throws IOException
    {
        File out = new File(input.getParent(),input.getName().replace(".", "_compressed."));
-      
+        try(
+               FileInputStream fis = new FileInputStream(input);
+               FileOutputStream fos = new FileOutputStream(out);
+               GZIPOutputStream gzip = new GZIPOutputStream(fos)
+           ) 
