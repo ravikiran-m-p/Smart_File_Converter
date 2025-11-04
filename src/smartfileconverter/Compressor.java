c2d9f73138c3afce2a62170c73c2aaa49e0f445d
MODALAVALASA RAVI KIRAN <ravikiranmodalavalasa@gmail.com>
2025-11-04 18:44:42 +0530

    Update Compressor.java

--- src/smartfileconverter/Compressor.java
+++ src/smartfileconverter/Compressor.java
@@ -7,5 +7,5 @@ public class Compressor
 {
    public File compressText(File input) throws IOException
    {
-
+       File out = new File(input.getParent(),input.getName().replace(".", "_compressed."));
       
