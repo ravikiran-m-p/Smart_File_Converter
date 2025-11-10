2841c4fbb9fdf1c45b60e50ea6f06bbda9e90af5
MODALAVALASA RAVI KIRAN <ravikiranmodalavalasa@gmail.com>
2025-11-10 19:56:20 +0530

    Update FileConverterUI.java
    neat clean code with strucured brackets

--- src/smartfileconverter/FileConverterUI.java
+++ src/smartfileconverter/FileConverterUI.java
@@ -54,7 +54,8 @@ public class FileConverterUI
             status.setText("<html> Done!<br>Saved: " + output.getAbsolutePath() + "</html>");
             openBtn.setVisible(true);
             openBtn.addActionListener(e -> {
-                try {
+                try
+                {
                     Desktop.getDesktop().open(output); 
                 }
                 catch (Exception ex) 
